68c1ba4e89acccdf717500d3ccd63ccb6268f402
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-27 14:54:56 +0100

    configuration

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+const path = require('path'); 
+dotenv.config();
+
+module.exports = {
+  env: {
+    REQUESRS_API_ENDPOINT_USERS: process.env.REQUESRS_API_ENDPOINT_USERS,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@customTypes'] = path.join(__dirname, 'Types');
+    config.resolve.alias['@api'] = path.join(__dirname, 'API');
+    config.resolve.alias['@icon'] = path.join(__dirname, 'app');
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'reqres.in'
+      }
+    ]
+  }
+};
