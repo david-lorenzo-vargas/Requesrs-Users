774f7f2f2dcf2868b1b6eee1a7cbf7ef7c965f53
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-27 14:59:42 +0100

    HomePage

---
+++ src/app/Components/Organisms/HomePage/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useEffect } from "react";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+const HomePage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    //we redirect the user to /users?page=1
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); 
+
+    router.replace(`/users?${params.toString()}`);
+  }, []);
+
+  return (
+    <></>
+  );
+};
+
+export default HomePage;
