b43816ae062431cc7fc996d8fc414f366b176113
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-28 09:49:20 +0100

    unnecesary file removed

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomePage from "./Components/Organisms/HomePage";
-
-export default function Home() {
-  return (
-    <main className="flex h-screen flex-row items-center justify-center bg-lightGray">
-      <HomePage />
-    </main>
-  );
-};
